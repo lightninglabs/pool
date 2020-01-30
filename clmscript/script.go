1b2aa96a51f860e769b3841a395febf7c0dba961
Wilmer Paulino <wilmer.paulino@gmail.com>
2020-01-29 16:18:45 -0800

    clmscript: add account witness script helpers

--- clmscript/script.go
+++ clmscript/script.go
@@ -17,11 +17,41 @@ const (
 	//
 	// TODO(wilmer): decide on actual value.
 	AccountKeyFamily keychain.KeyFamily = 220
+
+	// AccountWitnessScriptSize: 79 bytes
+	//	- OP_DATA: 1 byte (trader_key length)
+	//	- <trader_key>: 33 bytes
+	//	- OP_CHECKSIGVERIFY: 1 byte
+	//	- OP_DATA: 1 byte (auctioneer_key length)
+	//	- <auctioneer_key>: 33 bytes
+	//	- OP_CHECKSIG: 1 byte
+	//	- OP_IFDUP: 1 byte
+	//	- OP_NOTIF: 1 byte
+	//	- OP_DATA: 1 byte (account_expiry length)
+	//	- <account_expiry>: 4 bytes
+	//	- OP_CHECKLOCKTIMEVERIFY: 1 byte
+	//	- OP_ENDIF: 1 byte
+	AccountWitnessScriptSize = 1 + 33 + 1 + 1 + 33 + 1 + 1 + 1 + 1 + 4 + 1 + 1
+
+	// MultiSigWitnessSize: 227 bytes
+	//	- OP_DATA: 1 byte (trader_sig length)
+	//	- <trader_sig>: 73 bytes
+	//	- OP_DATA: 1 byte (auctioneer_sig length)
+	//	- <auctioneer_sig>: 73 bytes
+	//	- <witness_script>: 79 bytes
+	MultiSigWitnessSize = 1 + 73 + 1 + 73 + AccountWitnessScriptSize
+
+	// ExpiryWitnessSize: 154 bytes
+	//	- OP_DATA: 1 byte (trader_sig length)
+	//	- <trader_sig>: 73 bytes
+	//	- OP_DATA: 1 byte (nil length)
+	//	- <witness_script>: 79 bytes
+	ExpiryWitnessSize = 1 + 73 + 1 + AccountWitnessScriptSize
 )
 
 // TraderKeyTweak computes the tweak based on the current per-batch key and
-// shared secret that should be applied to an account's trader key. The tweak is
-// computed as the following:
+// shared secret that should be applied to an account's base trader key. The
+// tweak is computed as the following:
 //
 //	tweak = sha256(batchKey || secret || traderKey)
 func TraderKeyTweak(batchKey *btcec.PublicKey, secret [32]byte,
@@ -34,13 +64,23 @@ func TraderKeyTweak(batchKey *btcec.PublicKey, secret [32]byte,
 	return h.Sum(nil)
 }
 
-// AccountScript returns the witness script of an account on-chain.
+// AuctioneerKeyTweak computes the tweak based on the tweaked trader's key that
+// should be applied to an account's auctioneer base key. The tweak is computed
+// as the following:
 //
-// <trader_key> OP_CHECKSIGVERIFY
-// <auctioneer_key> OP_CHECKSIG OP_IFDUP OP_NOTIF
-//	<account_expiry> OP_CHECKLOCKTIMEVERIFY
-// OP_ENDIF
-func AccountScript(expiry uint32, traderKey, auctioneerKey,
+//	traderKeyTweak = sha256(batchKey || secret || traderKey)
+//	tweakedTraderKey = (traderKey + traderKeyTweak) * G
+//	auctioneerKeyTweak = sha256(tweakedTraderKey || auctioneerKey)
+func AuctioneerKeyTweak(traderKey, auctioneerKey, batchKey *btcec.PublicKey,
+	secret [32]byte) []byte {
+
+	traderKeyTweak := TraderKeyTweak(batchKey, secret, traderKey)
+	tweakedTraderKey := input.TweakPubKeyWithTweak(traderKey, traderKeyTweak)
+	return input.SingleTweakBytes(tweakedTraderKey, auctioneerKey)
+}
+
+// AccountWitnessScript returns the witness script of an account.
+func AccountWitnessScript(expiry uint32, traderKey, auctioneerKey,
 	batchKey *btcec.PublicKey, secret [32]byte) ([]byte, error) {
 
 	traderKeyTweak := TraderKeyTweak(batchKey, secret, traderKey)
@@ -61,11 +101,69 @@ func AccountScript(expiry uint32, traderKey, auctioneerKey,
 	builder.AddOp(txscript.OP_CHECKLOCKTIMEVERIFY)
 	builder.AddOp(txscript.OP_ENDIF)
 
-	script, err := builder.Script()
+	return builder.Script()
+}
+
+// AccountScript returns the output script of an account on-chain.
+//
+// <trader_key> OP_CHECKSIGVERIFY
+// <auctioneer_key> OP_CHECKSIG OP_IFDUP OP_NOTIF
+//	<account_expiry> OP_CHECKLOCKTIMEVERIFY
+// OP_ENDIF
+func AccountScript(expiry uint32, traderKey, auctioneerKey,
+	batchKey *btcec.PublicKey, secret [32]byte) ([]byte, error) {
+
+	witnessScript, err := AccountWitnessScript(
+		expiry, traderKey, auctioneerKey, batchKey, secret,
+	)
 	if err != nil {
 		return nil, err
 	}
-	return input.WitnessScriptHash(script)
+	return input.WitnessScriptHash(witnessScript)
+}
+
+// SpendExpiry returns the witness required to spend an account through the
+// expiration script path.
+func SpendExpiry(witnessScript, traderSig []byte) wire.TxWitness {
+	witness := make(wire.TxWitness, 3)
+	witness[0] = nil // Invalid auctioneer sig to force expiry path.
+	witness[1] = traderSig
+	witness[2] = witnessScript
+	return witness
+}
+
+// SpendMultiSig returns the witness required to spend an account through the
+// multi-sig script path.
+func SpendMultiSig(witnessScript, traderSig, auctioneerSig []byte) wire.TxWitness {
+	witness := make(wire.TxWitness, 3)
+	witness[0] = auctioneerSig
+	witness[1] = traderSig
+	witness[2] = witnessScript
+	return witness
+}
+
+// IsExpirySpend determines whether the provided witness corresponds to the
+// expiration script path of an account.
+func IsExpirySpend(witness wire.TxWitness) bool {
+	if len(witness) != 3 {
+		return false
+	}
+	if len(witness[0]) != 0 {
+		return false
+	}
+	return true
+}
+
+// IsMultiSigSpend determines whether the provided witness corresponds to the
+// multi-sig script path of an account.
+func IsMultiSigSpend(witness wire.TxWitness) bool {
+	if len(witness) != 3 {
+		return false
+	}
+	if len(witness[0]) == 0 {
+		return false
+	}
+	return true
 }
 
 // IncrementKey increments the given key by the backing curve's base point.
