3c14e92e11d15da846bbeef36604cf84f71d3754
Oliver Gugger <gugger@gmail.com>
2020-12-22 16:03:28 +0100

    poolrpc+order: group orders and prices by duration markets

--- order/rpc_parse.go
+++ order/rpc_parse.go
@@ -228,21 +228,45 @@ func ParseRPCBatch(prepareMsg *poolrpc.OrderMatchPrepare) (*Batch,
 		BatchTX:       &wire.MsgTx{},
 	}
 
-	// Parse matched orders.
-	for ourOrderHex, rpcMatchedOrders := range prepareMsg.MatchedOrders {
-		var ourOrder Nonce
-		ourOrderBytes, err := hex.DecodeString(ourOrderHex)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing nonce: %v", err)
+	// Parse matched orders market by market.
+	for leaseDuration, matchedMarket := range prepareMsg.MatchedMarkets {
+		orders := matchedMarket.MatchedOrders
+		for ourOrderHex, rpcMatchedOrders := range orders {
+			var ourOrder Nonce
+			ourOrderBytes, err := hex.DecodeString(ourOrderHex)
+			if err != nil {
+				return nil, fmt.Errorf("error parsing nonce: "+
+					"%v", err)
+			}
+			copy(ourOrder[:], ourOrderBytes)
+			matchedOrders, err := ParseRPCMatchedOrders(
+				rpcMatchedOrders,
+			)
+			if err != nil {
+				return nil, fmt.Errorf("error parsing matched "+
+					"order: %v", err)
+			}
+
+			// It is imperative that all matched orders have the
+			// correct lease duration. Otherwise assumptions in the
+			// batch verifier won't be correct. That's why we
+			// validate this here already.
+			for _, matchedOrder := range matchedOrders {
+				details := matchedOrder.Order.Details()
+				if details.LeaseDuration != leaseDuration {
+					return nil, fmt.Errorf("matched order "+
+						"%s has incorrect lease "+
+						"duration %d for bucket %d",
+						details.Nonce(),
+						details.LeaseDuration,
+						leaseDuration)
+				}
+			}
+
+			b.MatchedOrders[ourOrder] = matchedOrders
 		}
-		copy(ourOrder[:], ourOrderBytes)
-		b.MatchedOrders[ourOrder], err = ParseRPCMatchedOrders(
-			rpcMatchedOrders,
-		)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing matched order: "+
-				"%v", err)
-		}
+
+		// TODO(guggero): Add clearing price per lease duration bucket.
 	}
 
 	// Parse account diff.
