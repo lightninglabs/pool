a6f5c600203211eafdd84f1753e0b700fb466470
Oliver Gugger <gugger@gmail.com>
2021-03-19 11:50:03 +0100

    sidecar: add signing and verification functions
    For maximum re-usability of the signing/verifying code, we add them as
    helper methods to the sidecar package itself.

--- sidecar/verification.go
+++ sidecar/verification.go
@@ -1,10 +1,138 @@
 package sidecar
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/btcsuite/btcutil"
+	"github.com/lightninglabs/lndclient"
+	"github.com/lightningnetwork/lnd/keychain"
+	"github.com/lightningnetwork/lnd/lnwire"
 )
+
+// SignOffer adds a signature over the offer digest to the given ticket.
+func SignOffer(ctx context.Context, ticket *Ticket,
+	signingKeyLoc keychain.KeyLocator, signer lndclient.SignerClient) error {
+
+	// The ticket needs to be in the correct state for us to sign it.
+	if ticket == nil || ticket.State < StateOffered {
+		return fmt.Errorf("ticket is in invalid state")
+	}
+
+	// The ticket also needs to have a signed offer.
+	offer := ticket.Offer
+	if offer.SignPubKey == nil || offer.SigOfferDigest != nil {
+		return fmt.Errorf("offer in ticket is not in expected state " +
+			"to be signed")
+	}
+
+	// Let's sign the offer part of the ticket with our node's identity key
+	// now.
+	offerDigest, err := ticket.OfferDigest()
+	if err != nil {
+		return fmt.Errorf("error digesting offer: %v", err)
+	}
+	rawSig, err := signer.SignMessage(ctx, offerDigest[:], signingKeyLoc)
+	if err != nil {
+		return fmt.Errorf("error signing offer: %v", err)
+	}
+	wireSig, err := lnwire.NewSigFromRawSignature(rawSig)
+	if err != nil {
+		return fmt.Errorf("error parsing raw signature: %v", err)
+	}
+	ecSig, err := wireSig.ToSignature()
+	if err != nil {
+		return fmt.Errorf("error parsing EC signature: %v", err)
+	}
+	ticket.Offer.SigOfferDigest = ecSig
+
+	return nil
+}
+
+// VerifyOffer verifies the state of a ticket to be in the offered state and
+// also makes sure the offer signature is valid.
+func VerifyOffer(ctx context.Context, ticket *Ticket,
+	signer lndclient.SignerClient) error {
+
+	// The ticket needs to be in the correct state for us to verify it.
+	if ticket == nil || ticket.State < StateOffered {
+		return fmt.Errorf("ticket is in invalid state")
+	}
+
+	// The ticket also needs to have a signed offer.
+	offer := ticket.Offer
+	if offer.SignPubKey == nil || offer.SigOfferDigest == nil {
+		return fmt.Errorf("offer in ticket is not signed")
+	}
+
+	var offerPubKeyRaw [33]byte
+	copy(offerPubKeyRaw[:], ticket.Offer.SignPubKey.SerializeCompressed())
+
+	// Make sure the provider's signature over the offer is valid.
+	offerDigest, err := ticket.OfferDigest()
+	if err != nil {
+		return fmt.Errorf("error calculating offer digest: %v", err)
+	}
+	sigValid, err := signer.VerifyMessage(
+		ctx, offerDigest[:], offer.SigOfferDigest.Serialize(),
+		offerPubKeyRaw,
+	)
+	if err != nil {
+		return fmt.Errorf("unable to verify offer signature: %v", err)
+	}
+	if !sigValid {
+		return fmt.Errorf("signature not valid for public key %x",
+			offerPubKeyRaw[:])
+	}
+
+	return nil
+}
+
+// SignOrder adds the order part to a ticket and signs it, adding the signature
+// as well.
+func SignOrder(ctx context.Context, ticket *Ticket, bidNonce [32]byte,
+	signingKeyLoc keychain.KeyLocator, signer lndclient.SignerClient) error {
+
+	// The ticket needs to be in the correct state for us to sign it.
+	if ticket == nil || ticket.State < StateRegistered {
+		return fmt.Errorf("ticket is in invalid state")
+	}
+
+	// The ticket also needs to have a signed offer.
+	offer := ticket.Offer
+	if offer.SignPubKey == nil || offer.SigOfferDigest == nil {
+		return fmt.Errorf("offer in ticket is not signed")
+	}
+
+	// Add the bid order's nonce to the order part of the ticket now.
+	if ticket.Order == nil {
+		ticket.Order = &Order{}
+	}
+	ticket.Order.BidNonce = bidNonce
+	ticket.State = StateOrdered
+
+	// Let's sign the order part of the ticket with our node's identity key
+	// now.
+	orderDigest, err := ticket.OrderDigest()
+	if err != nil {
+		return fmt.Errorf("error digesting order: %v", err)
+	}
+	rawSig, err := signer.SignMessage(ctx, orderDigest[:], signingKeyLoc)
+	if err != nil {
+		return fmt.Errorf("error signing order: %v", err)
+	}
+	wireSig, err := lnwire.NewSigFromRawSignature(rawSig)
+	if err != nil {
+		return fmt.Errorf("error parsing raw signature: %v", err)
+	}
+	ecSig, err := wireSig.ToSignature()
+	if err != nil {
+		return fmt.Errorf("error parsing EC signature: %v", err)
+	}
+	ticket.Order.SigOrderDigest = ecSig
+
+	return nil
+}
 
 // CheckOfferParams makes sure the offer parameters of a sidecar ticket are
 // valid and sane.
---
+++ sidecar/verification_test.go
@@ -0,0 +1,246 @@
+package sidecar
+
+import (
+	"context"
+	"math/big"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/lightninglabs/pool/internal/test"
+	"github.com/lightningnetwork/lnd/keychain"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	_, providerPubKey = btcec.PrivKeyFromBytes(btcec.S256(), []byte{0x02})
+	testOfferSig      = &btcec.Signature{
+		R: new(big.Int).SetInt64(44),
+		S: new(big.Int).SetInt64(22),
+	}
+)
+
+// TestSignOffer makes sure that a sidecar ticket's offer part can be signed
+// correctly.
+func TestSignOffer(t *testing.T) {
+	t.Parallel()
+
+	mockSigner := test.NewMockSigner()
+	mockSigner.Signature = testOfferSig.Serialize()
+
+	testCases := []struct {
+		name        string
+		ticket      *Ticket
+		expectedErr string
+	}{{
+		name:        "no ticket",
+		expectedErr: "ticket is in invalid state",
+	}, {
+		name: "non offered ticket",
+		ticket: &Ticket{
+			State: StateCreated,
+		},
+		expectedErr: "ticket is in invalid state",
+	}, {
+		name: "offered ticket with no sign pubkey",
+		ticket: &Ticket{
+			State: StateOffered,
+			Offer: Offer{},
+		},
+		expectedErr: "offer in ticket is not in expected state to be",
+	}, {
+		name: "offered ticket has signature",
+		ticket: &Ticket{
+			ID:    [8]byte{1, 2, 3, 4},
+			State: StateOffered,
+			Offer: Offer{
+				SignPubKey:     providerPubKey,
+				SigOfferDigest: testOfferSig,
+			},
+		},
+		expectedErr: "offer in ticket is not in expected state to be",
+	}, {
+		name: "all valid",
+		ticket: &Ticket{
+			ID:    [8]byte{1, 2, 3, 4},
+			State: StateOffered,
+			Offer: Offer{
+				SignPubKey: providerPubKey,
+			},
+		},
+		expectedErr: "",
+	}}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		if tc.ticket != nil {
+			digest, err := tc.ticket.OfferDigest()
+			require.NoError(t, err)
+			mockSigner.SignatureMsg = string(digest[:])
+		}
+
+		err := SignOffer(
+			context.Background(), tc.ticket, keychain.KeyLocator{},
+			mockSigner,
+		)
+
+		if tc.expectedErr == "" {
+			require.NoError(t, err)
+			require.Equal(
+				t, testOfferSig, tc.ticket.Offer.SigOfferDigest,
+			)
+		} else {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tc.expectedErr)
+		}
+	}
+}
+
+// TestVerifyOffer makes sure that a sidecar ticket's offer part can be verified
+// correctly.
+func TestVerifyOffer(t *testing.T) {
+	t.Parallel()
+
+	mockSigner := test.NewMockSigner()
+	mockSigner.Signature = testOfferSig.Serialize()
+
+	testCases := []struct {
+		name        string
+		ticket      *Ticket
+		expectedErr string
+	}{{
+		name:        "no ticket",
+		expectedErr: "ticket is in invalid state",
+	}, {
+		name: "non offered ticket",
+		ticket: &Ticket{
+			State: StateCreated,
+		},
+		expectedErr: "ticket is in invalid state",
+	}, {
+		name: "offered ticket with no sign pubkey",
+		ticket: &Ticket{
+			State: StateOffered,
+			Offer: Offer{},
+		},
+		expectedErr: "offer in ticket is not signed",
+	}, {
+		name: "invalid sig",
+		ticket: &Ticket{
+			ID:    [8]byte{1, 2, 3, 4},
+			State: StateOffered,
+			Offer: Offer{
+				SignPubKey: providerPubKey,
+				SigOfferDigest: &btcec.Signature{
+					R: new(big.Int).SetInt64(33),
+					S: new(big.Int).SetInt64(33),
+				},
+			},
+		},
+		expectedErr: "signature not valid for public key",
+	}, {
+		name: "all valid",
+		ticket: &Ticket{
+			ID:    [8]byte{1, 2, 3, 4},
+			State: StateOffered,
+			Offer: Offer{
+				SignPubKey:     providerPubKey,
+				SigOfferDigest: testOfferSig,
+			},
+		},
+		expectedErr: "",
+	}}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		if tc.ticket != nil {
+			digest, err := tc.ticket.OfferDigest()
+			require.NoError(t, err)
+			mockSigner.SignatureMsg = string(digest[:])
+		}
+
+		err := VerifyOffer(
+			context.Background(), tc.ticket, mockSigner,
+		)
+
+		if tc.expectedErr == "" {
+			require.NoError(t, err)
+		} else {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tc.expectedErr)
+		}
+	}
+}
+
+// TestSignOrder makes sure that a sidecar ticket's order part can be signed
+// correctly.
+func TestSignOrder(t *testing.T) {
+	t.Parallel()
+
+	mockSigner := test.NewMockSigner()
+	mockSigner.Signature = testOfferSig.Serialize()
+
+	testCases := []struct {
+		name        string
+		ticket      *Ticket
+		expectedErr string
+	}{{
+		name:        "no ticket",
+		expectedErr: "ticket is in invalid state",
+	}, {
+		name: "non registered ticket",
+		ticket: &Ticket{
+			State: StateCreated,
+		},
+		expectedErr: "ticket is in invalid state",
+	}, {
+		name: "non signed offer",
+		ticket: &Ticket{
+			State: StateRegistered,
+			Offer: Offer{
+				SignPubKey: providerPubKey,
+			},
+		},
+		expectedErr: "offer in ticket is not signed",
+	}, {
+		name: "all valid",
+		ticket: &Ticket{
+			ID:    [8]byte{1, 2, 3, 4},
+			State: StateRegistered,
+			Offer: Offer{
+				SignPubKey:     providerPubKey,
+				SigOfferDigest: testOfferSig,
+			},
+		},
+		expectedErr: "",
+	}}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		if tc.ticket != nil {
+			digest, err := tc.ticket.OfferDigest()
+			require.NoError(t, err)
+			mockSigner.SignatureMsg = string(digest[:])
+		}
+
+		err := SignOrder(
+			context.Background(), tc.ticket, [32]byte{},
+			keychain.KeyLocator{}, mockSigner,
+		)
+
+		if tc.expectedErr == "" {
+			require.NoError(t, err)
+
+			require.NotNil(t, tc.ticket.Order)
+			require.Equal(
+				t, testOfferSig, tc.ticket.Order.SigOrderDigest,
+			)
+			require.Equal(t, StateOrdered, tc.ticket.State)
+		} else {
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tc.expectedErr)
+		}
+	}
+}
