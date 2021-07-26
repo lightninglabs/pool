acda1cd4791e3ecd210edf2995790ceefbcf167d
Oliver Gugger <gugger@gmail.com>
2021-07-26 18:39:16 +0200

    poolrpc: generate JSON/WASM client stubs

--- poolrpc/gen_protos.sh
+++ poolrpc/gen_protos.sh
@@ -32,6 +32,24 @@ function generate() {
       --openapiv2_opt grpc_api_configuration=${annotationsFile} \
       --openapiv2_opt json_names_for_fields=false \
       "${file}"
+
+    # Generate the JSON/WASM client stubs.
+    falafel=$(which falafel)
+    pkg="poolrpc"
+    manual_import="github.com/lightninglabs/pool/auctioneerrpc"
+    opts="package_name=$pkg,manual_import=$manual_import,api_prefix=1,js_stubs=1,build_tags=// +build js"
+    protoc -I/usr/local/include -I. -I.. \
+           --plugin=protoc-gen-custom=$falafel\
+           --custom_out=. \
+           --custom_opt="$opts" \
+           trader.proto
+
+    # We've got a bit of a weird setup with the same RPC package being split over
+    # two golang packages. We can't really fix this without breaking a lot so it's
+    # easier to fix stuff explicitly here.
+    sed -i 's/BatchSnapshotRequest/auctioneerrpc.BatchSnapshotRequest/g' trader.pb.json.go
+    sed -i 's/BatchSnapshotsRequest/auctioneerrpc.BatchSnapshotsRequest/g' trader.pb.json.go
+
   done
 }
 
