ffbda094241f5d669d82d36b2a0bac21e34a62d6
positiveblue <jordi.montes.sanabria@gmail.com>
2021-12-09 16:53:36 -0800

    account: generate mocks for `account/interfaces.go`

--- gen.go
+++ gen.go
@@ -7,5 +7,6 @@ package pool
 
 //go:generate mockgen -source=sidecar/interfaces.go -package=sidecar -destination=sidecar/mock_interfaces.go
 //go:generate mockgen -source=internal/test/interfaces.go -package=test -destination=internal/test/mock_interfaces.go
+//go:generate mockgen -source=account/interfaces.go -package=account -destination=account/mock_interfaces.go
 //go:generate mockgen -source=account/watcher/interfaces.go -package=watcher -destination=account/watcher/mock_interface_test.go
 //go:generate mockgen -source=order/interfaces.go -package=order -destination=order/mock_interfaces.go
