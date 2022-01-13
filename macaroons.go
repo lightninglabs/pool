599f26528c3b57db3905fbfe34d6600433b45304
Elle Mouton <elle.mouton@gmail.com>
2022-01-13 13:58:59 +0200

    multi: use lndclient MacaroonService
    Since the code for creating and using a macaroon service is the same for
    multiple projects (pool, loop, litd etc), the code has been unified in
    lndclient. So this commit removes the macaroon service code and instead
    uses the lndclient code.

--- macaroons.go
+++ macaroons.go
@@ -1,18 +1,6 @@
 package pool
 
 import (
-	"context"
-	"fmt"
-	"io/ioutil"
-	"os"
-
-	"github.com/lightninglabs/pool/clientdb"
-	"github.com/lightningnetwork/lnd/kvdb"
-	"github.com/lightningnetwork/lnd/lnrpc"
-	"github.com/lightningnetwork/lnd/macaroons"
-	"github.com/lightningnetwork/lnd/rpcperms"
-	"go.etcd.io/bbolt"
-	"google.golang.org/grpc"
 	"gopkg.in/macaroon-bakery.v2/bakery"
 )
 
@@ -153,30 +141,6 @@ var (
 		}},
 	}
 
-	// allPermissions is the list of all existing permissions that exist
-	// for poold's RPC. The default macaroon that is created on startup
-	// contains all these permissions and is therefore equivalent to lnd's
-	// admin.macaroon but for pool.
-	allPermissions = []bakery.Op{{
-		Entity: "account",
-		Action: "read",
-	}, {
-		Entity: "account",
-		Action: "write",
-	}, {
-		Entity: "order",
-		Action: "read",
-	}, {
-		Entity: "order",
-		Action: "write",
-	}, {
-		Entity: "auction",
-		Action: "read",
-	}, {
-		Entity: "auth",
-		Action: "read",
-	}}
-
 	// macDbDefaultPw is the default encryption password used to encrypt the
 	// pool macaroon database. The macaroon service requires us to set a
 	// non-nil password so we set it to an empty string. This will cause the
@@ -188,121 +152,3 @@ var (
 	// though.
 	macDbDefaultPw = []byte("")
 )
-
-// startMacaroonService starts the macaroon validation service, creates or
-// unlocks the macaroon database and creates the default macaroon if it doesn't
-// exist yet. If macaroons are disabled in general in the configuration, none of
-// these actions are taken.
-func (s *Server) startMacaroonService(createDefaultMacaroonFile bool) error {
-	var err error
-	s.macaroonDB, err = kvdb.GetBoltBackend(&kvdb.BoltBackendConfig{
-		DBPath:     s.cfg.BaseDir,
-		DBFileName: "macaroons.db",
-		DBTimeout:  clientdb.DefaultPoolDBTimeout,
-	})
-	if err == bbolt.ErrTimeout {
-		return fmt.Errorf("error while trying to open %s/%s: "+
-			"timed out after %v when trying to obtain exclusive "+
-			"lock - make sure no other pool daemon process "+
-			"(standalone or embedded in lightning-terminal) is "+
-			"running", s.cfg.BaseDir, "macaroons.db",
-			clientdb.DefaultPoolDBTimeout)
-	}
-	if err != nil {
-		return fmt.Errorf("unable to load macaroon db: %v", err)
-	}
-
-	// Create the macaroon authentication/authorization service.
-	s.macaroonService, err = macaroons.NewService(
-		s.macaroonDB, poolMacaroonLocation, false,
-		macaroons.IPLockChecker,
-	)
-	if err != nil {
-		return fmt.Errorf("unable to set up macaroon service: %v", err)
-	}
-
-	// Try to unlock the macaroon store with the private password.
-	err = s.macaroonService.CreateUnlock(&macDbDefaultPw)
-	if err != nil {
-		return fmt.Errorf("unable to unlock macaroon DB: %v", err)
-	}
-
-	// There are situations in which we don't want a macaroon to be created
-	// on disk (for example when running inside LiT stateless integrated
-	// mode). For any other cases, we create macaroon files for the pool CLI
-	// in the default directory.
-	if createDefaultMacaroonFile && !lnrpc.FileExists(s.cfg.MacaroonPath) {
-		// We don't offer the ability to rotate macaroon root keys yet,
-		// so just use the default one since the service expects some
-		// value to be set.
-		idCtx := macaroons.ContextWithRootKeyID(
-			context.Background(), macaroons.DefaultRootKeyID,
-		)
-
-		// We only generate one default macaroon that contains all
-		// existing permissions (equivalent to the admin.macaroon in
-		// lnd). Custom macaroons can be created through the bakery
-		// RPC.
-		poolMac, err := s.macaroonService.Oven.NewMacaroon(
-			idCtx, bakery.LatestVersion, nil, allPermissions...,
-		)
-		if err != nil {
-			return err
-		}
-		poolMacBytes, err := poolMac.M().MarshalBinary()
-		if err != nil {
-			return err
-		}
-		err = ioutil.WriteFile(s.cfg.MacaroonPath, poolMacBytes, 0644)
-		if err != nil {
-			if err := os.Remove(s.cfg.MacaroonPath); err != nil {
-				log.Errorf("Unable to remove %s: %v",
-					s.cfg.MacaroonPath, err)
-			}
-			return err
-		}
-	}
-
-	return nil
-}
-
-// stopMacaroonService closes the macaroon database.
-func (s *Server) stopMacaroonService() error {
-	var shutdownErr error
-	if err := s.macaroonService.Close(); err != nil {
-		log.Errorf("Error closing macaroon service: %v", err)
-		shutdownErr = err
-	}
-
-	if err := s.macaroonDB.Close(); err != nil {
-		log.Errorf("Error closing macaroon DB: %v", err)
-		shutdownErr = err
-	}
-
-	return shutdownErr
-}
-
-// macaroonInterceptor creates gRPC server options with the macaroon security
-// interceptors.
-func (s *Server) macaroonInterceptor() (grpc.UnaryServerInterceptor,
-	grpc.StreamServerInterceptor, error) {
-
-	interceptor := rpcperms.NewInterceptorChain(log, false, nil)
-	err := interceptor.Start()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	interceptor.SetWalletUnlocked()
-	interceptor.AddMacaroonService(s.macaroonService)
-	for method, permissions := range RequiredPermissions {
-		err := interceptor.AddPermission(method, permissions)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-
-	unaryInterceptor := interceptor.MacaroonUnaryServerInterceptor()
-	streamInterceptor := interceptor.MacaroonStreamServerInterceptor()
-	return unaryInterceptor, streamInterceptor, nil
-}
