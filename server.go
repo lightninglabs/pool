a5539ee08afd4e86d2d25c66298b788c26458db7
Oliver Gugger <gugger@gmail.com>
2023-01-30 17:08:24 +0100

    server+README: bump minimum required lnd version to v0.15.4-beta
    Since any version prior to v0.15.4-beta will not sync to the mainnet
    chain, we can safely bump the minimum required version to that since no
    older version will be running (properly) anywhere.

--- server.go
+++ server.go
@@ -45,7 +45,7 @@ var (
 	minimalCompatibleVersion = &verrpc.Version{
 		AppMajor: 0,
 		AppMinor: 15,
-		AppPatch: 1,
+		AppPatch: 4,
 
 		// We don't actually require the invoicesrpc calls. But if we
 		// try to use lndclient on an lnd that doesn't have it enabled,
